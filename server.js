8a5c045618a2dd65384385e2ef23c7c48f700b36
Mahfuz1907 <mahfuztamim1907@gmail.com>
2026-06-04 19:02:39 +0600

    cors update

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://portfolio-frontend-cv.netlify.app/', 'http://localhost:5173']
+}));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
